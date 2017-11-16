02b433cd0c5a16b1d63b9fca672cf2f0f51f4705
dvirtz <dvirtz@gmail.com>
2017-11-16 18:01:07 +0200

    restore compilation

--- lib/Target/LEG/LEGInstrInfo.h
+++ lib/Target/LEG/LEGInstrInfo.h
@@ -22,15 +22,12 @@
 
 namespace llvm {
 
-class LEGSubtarget;
-
 class LEGInstrInfo : public LEGGenInstrInfo {
   const LEGRegisterInfo RI;
-  const LEGSubtarget& ST;
   virtual void anchor();
 
 public:
-  LEGInstrInfo(LEGSubtarget& ST);
+  LEGInstrInfo();
 
   /// getRegisterInfo - TargetInstrInfo is a superset of MRegister info.  As
   /// such, whenever a client has an instance of instruction info, it should
--- lib/Target/LEG/LEGSubtarget.cpp
+++ lib/Target/LEG/LEGSubtarget.cpp
@@ -29,8 +29,10 @@ LEGSubtarget::LEGSubtarget(const Triple &TT, StringRef CPU, StringRef FS,
                            LEGTargetMachine &TM)
     : LEGGenSubtargetInfo(TT, CPU, FS),
       DL("e-m:e-p:32:32-i1:8:32-i8:8:32-i16:16:32-i64:32-f64:32-a:0:32-n32"),
-      InstrInfo(initializeSubtargetDependencies(CPU, FS)), TLInfo(TM), TSInfo(),
-      FrameLowering() {}
+      InstrInfo(), TLInfo(TM), TSInfo(), FrameLowering()
+{
+  initializeSubtargetDependencies(CPU, FS);
+}
 
 LEGSubtarget &LEGSubtarget::initializeSubtargetDependencies(StringRef CPU,
                                                             StringRef FS) {
